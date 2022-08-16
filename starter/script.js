ea0e3c5577e61c4f9d0d1302be563a4ef45b55a8
Reneta <renkagh55@gmail.com>
2022-08-16 13:16:56 +0300

    initial commit

---
+++ starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// elements
+
+let againgElement = document.querySelector('.again');
+let numValueElement = document.querySelector('.guess');
+let checkElement = document.querySelector('.check');
+let messageElement = document.querySelector('.message');
+let scoreElement = document.querySelector('.label-score');
+let highscoreElement = document.querySelector('.label-highscore');
+
+// input
